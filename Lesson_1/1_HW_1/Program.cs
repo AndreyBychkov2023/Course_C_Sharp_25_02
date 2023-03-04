16dee32d69cb694f5a92c98f714bf8ede6f260f7
Andrey <bychkov80@mail.ru>
2023-03-04 16:46:32 +0700

    Task 1_HW_1

---
+++ Lesson_1/1_HW_1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 1: Напишите программу, которая на вход
+//принимает два числа и выдаёт, какое число
+//большее, а какое меньшее.
+
+string s_a = Console.ReadLine()!;
+string s_b = Console.ReadLine()!;
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if (a > b)
+{
+   Console.Write("max = ");
+   Console.WriteLine(s_a);
+}
+
+else if (a < b)
+{
+   Console.Write("max = ");
+   Console.WriteLine(s_b);
+}
+
+else 
+{
+    Console.Write("Числа равны между собой");
+}
