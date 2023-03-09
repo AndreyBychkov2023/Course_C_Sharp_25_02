79968775885cfaf4ae8485d03af0ff260531433c
Andrey <bychkov80@mail.ru>
2023-03-09 14:10:53 +0700

    Task_4_1

---
+++ Lesson_4/4_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3   78 -> 2   89126 -> 5.
+
+int SumNum(int num)
+{
+    int kol = 0;
+    for (int i = 1; num > 0; i++)
+    {
+        num = num / 10;
+        kol = i;
+    }
+    return kol;
+}
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNum(A));
