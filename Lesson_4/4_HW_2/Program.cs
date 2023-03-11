6b1615fffd11c39e4b9f69681c5b48c6fd96b72c
Andrey <bychkov80@mail.ru>
2023-03-11 11:06:54 +0700

    Task_4_HW_2

---
+++ Lesson_4/4_HW_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11   82 -> 10   9012 -> 12.
+
+
+int SumNum(int num)
+{
+int result = 0;
+for (int i = 0; num > 0; i++)
+{
+    result += num % 10;
+    num = num / 10;
+}
+return result;
+
+}
+int A = int.Parse(Console.ReadLine()!);
+int result = SumNum(A);
+Console.WriteLine(result);
+
+
+
+
+
