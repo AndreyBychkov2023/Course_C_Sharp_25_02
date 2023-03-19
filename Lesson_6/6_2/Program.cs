877962f71b49c4762ac9b51922189388ac20ca01
Andrey Bychkov <bychkov80@mail.ru>
2023-03-19 08:16:18 +0500

    Task_6_2

---
+++ Lesson_6/6_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+string convert(int num)
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2;
+    }
+    return result;
+}
+int num = int.Parse(Console.ReadLine()!);
+string result = convert(num);
+Console.WriteLine(result);
