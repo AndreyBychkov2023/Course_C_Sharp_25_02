6e3deb785c24db70e25edb40008576e28c52243f
Andrey Bychkov <bychkov80@mail.ru>
2023-03-20 17:29:08 +0500

    Task_7_3

---
+++ Lesson_7/7_3/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 3: Задайте двумерный массив. Найдите сумму элементов
+// главной диагонали.
+// Например, задан массив:
+// 1 4 7
+// 5 9 2
+// 8 4 2
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+     
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+    
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+    
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+int SumMass (int [,] arr)
+{
+    int summarr = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == j) summarr = summarr + arr[i, j];
+        }
+    return summarr;
+}
+
+int num_row = int.Parse(Console.ReadLine()!);
+int num_column = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(num_row, num_column, start, stop);
+Print(mass);
+int sum = SumMass(mass);
+Console.WriteLine($"Summa: {sum}");
