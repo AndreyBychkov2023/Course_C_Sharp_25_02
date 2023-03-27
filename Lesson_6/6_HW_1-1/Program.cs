0ef9530b0035a164e3c07d5bf72dc5d3da430f32
Andrey Bychkov <bychkov80@mail.ru>
2023-03-27 19:54:42 +0500

    Task_6_HW_1-1

---
+++ Lesson_6/6_HW_1-1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int CountNum()
+{
+    int count = 0;
+    string word;
+
+    while (true)
+    {
+        Console.Write("Output any number: ");
+        word = Console.ReadLine()!;
+
+        if (word == "") return count;
+        else if (int.Parse(word) > 0) count += 1;
+    }
+}
+
+int count = CountNum();
+Console.WriteLine($"Numbers greater than zero: {count}");
