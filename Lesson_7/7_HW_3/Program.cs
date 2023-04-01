294fe9794ee60bb4fc870c0b6e0576f4dfde3df4
Andrey Bychkov <bychkov80@mail.ru>
2023-04-01 13:29:23 +0500

    Task_7_HW_3

--- Lesson_7/7_HW_3/Program.cs
+++ Lesson_7/7_HW_3/Program.cs
@@ -7,15 +7,6 @@
 // Среднее арифметическое каждого
 // столбца: 4,6; 5,6; 3,6; 3.
 
-
-// Задача 3: Задайте двумерный массив. Найдите сумму элементов
-// главной диагонали.
-// Например, задан массив:
-// 1 4 7
-// 5 9 2
-// 8 4 2
-// Сумма элементов главной диагонали: 1+9+2 = 12
-
 void Print(int[,] arr)
 {
     int row = arr.GetLength(0);
@@ -37,31 +28,34 @@ int[,] MassNums(int row, int column, int from, int to)
     for (int i = 0; i < row; i++)
         for (int j = 0; j < column; j++)
             arr[i, j] = new Random().Next(from, to);
-
     return arr;
 }
 
-int SumMass (int [,] arr)
+void SumMass(int [,] arr)
 {
-    int summarr = 0;
-    for (int i = 0; i < arr.GetLength(0); i++)
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            if (j == 0) summarr = summarr + arr[i, j];
-        }
-    return summarr;
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    double res;
+
+    for (int i = 0; i < column; i++)
+    {
+        res = 0;
+        for (int j = 0; j < row; j++) res += arr[j, i];
+        Console.Write($"{Math.Round(res / row, 2)}; ");
+    }
 }
 
 Console.WriteLine("Введите количество строк массива");    
-int num_row = int.Parse(Console.ReadLine()!);
+int row_num = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите количество колонок массива");
-int num_column = int.Parse(Console.ReadLine()!);
-int num_row = int.Parse(Console.ReadLine()!);
-int num_column = int.Parse(Console.ReadLine()!);
-int start = int.Parse(Console.ReadLine()!);
-int stop = int.Parse(Console.ReadLine()!);
+int column_num = int.Parse(Console.ReadLine()!);
 
-int[,] mass = MassNums(num_row, num_column, start, stop);
+Console.WriteLine("Задайте минимальное число в массиве");
+int start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Задайте максимальное число в массиве");
+int stop = int.Parse(Console.ReadLine()!);
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
 Print(mass);
-int sum = SumMass(mass);
-Console.WriteLine($"Summa: {sum}");
+
+SumMass(mass);
